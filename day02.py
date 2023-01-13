49a093d570d4e9672077b1a305d7a992ff90e739
kiminha983 <kiminha983@gmail.com>
2023-01-13 15:25:57 +0900

    day02 if random

--- day02.py
+++ day02.py
@@ -1,27 +1,9 @@
-# Chap 4 if
+import random
 
 limits = 20
-tweets = "pass" * 7
+tweets = "pass" * random.randint(1, 10)  # 1에서 10 사이의 정수가 임의로 발생
 diff = limits - len(tweets)
 if diff >= 0:
     print(tweets)
 else:
     print(f'글자 수 {abs(diff)}초과')
-
-
-vowels = 'aeiou'
-letter = 'u'
-if letter not in vowels:
-    print("실행 안됨!")
-
-
-a = []
-print(bool(a))
-a.append(5)
-print(bool(a))
-print(bool(set()))
-print(bool(dict()))
-print(bool(""))
-
-
-
