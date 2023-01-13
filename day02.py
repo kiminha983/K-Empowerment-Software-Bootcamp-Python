60bfd2ce63be5255cb70e7c3672129662dd02a29
kiminha983 <kiminha983@gmail.com>
2023-01-13 14:42:53 +0900

    day02 number2

--- day02.py
+++ day02.py
@@ -1,16 +1,15 @@
 # Chap 3
 
-print(2*2*2*2*2)
-print(2**5)
-print(pow(2, 5))
-print(divmod(9, 5))
-print(type(divmod(9, 5)))
-print(type((1, 2)))
-test = 1, 2  # packing
-print(type(test))
-print(test)
-print(test[1])
-# a, b, c = test  # unpacking
-a, b = test  # unpacking
-print(a)
-print(b)
+
+# number = 0b10011010
+# number = 0x9A
+# number = 0o232
+# print(number)
+
+number = 154
+print(bin(number))
+print(hex(number))
+print(oct(number))
+
+print(ord("a"))  # ascii
+print(hex(ord(" ")))  #  0x20
