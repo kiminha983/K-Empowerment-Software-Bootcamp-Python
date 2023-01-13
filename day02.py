0e110b004fc4fa35e6c591511f45f387e2f300bf
kiminha983 <kiminha983@gmail.com>
2023-01-13 15:22:13 +0900

    day02 if

--- day02.py
+++ day02.py
@@ -1,15 +1,27 @@
-# Chap 3
+# Chap 4 if
+
+limits = 20
+tweets = "pass" * 7
+diff = limits - len(tweets)
+if diff >= 0:
+    print(tweets)
+else:
+    print(f'글자 수 {abs(diff)}초과')
 
 
-# number = 0b10011010
-# number = 0x9A
-# number = 0o232
-# print(number)
+vowels = 'aeiou'
+letter = 'u'
+if letter not in vowels:
+    print("실행 안됨!")
 
-number = 154
-print(bin(number))
-print(hex(number))
-print(oct(number))
 
-print(ord("a"))  # ascii
-print(hex(ord(" ")))  #  0x20
+a = []
+print(bool(a))
+a.append(5)
+print(bool(a))
+print(bool(set()))
+print(bool(dict()))
+print(bool(""))
+
+
+
