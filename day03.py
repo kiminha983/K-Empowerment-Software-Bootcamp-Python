dc5cb825e313b7ade7c006397b72b16f05e3606a
kiminha983 <kiminha983@gmail.com>
2023-01-16 14:35:18 +0900

    day03 prime number v0.3 loopcounts ==

--- day03.py
+++ day03.py
@@ -1,13 +1,14 @@
-# prime number v0.2
+# prime number v0.3
 
 number = int(input("input number : "))
 counts = 0
 
-for k in range(1, number+1):
+for k in range(2, number):
     if number % k == 0:
         counts = counts + 1
 
-if counts == 2:
-    print(f'{number} is prime number!')
-else:
+if counts:
     print(f'{number} is NOT prime number.')
+else:
+    print(f'{number} is prime number!')
+
