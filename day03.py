d1374940abdaca70caff05105756ee488fdb57dd
kiminha983 <kiminha983@gmail.com>
2023-01-16 11:44:56 +0900

    day03 text04

--- day03.py
+++ day03.py
@@ -1,29 +1,6 @@
-subjects = ' $ python, data structure, database    $$$'
-print(subjects.title())
-print(subjects.count('data'))
-print(subjects.rfind('data'), subjects.rindex('data'))
-print(subjects.find('data'), subjects.index('data'))
-print(subjects.find('inha'))  # -1 return
-# print(subjects.index('inha'))  # Exception!
+song = """When an eel grabs your arm,
+And it causes great harm,
+That's - a moray!"""
 
-# subjects = ' $ python, data structure, database    $$$'
-# print(subjects.strip('$'))
-#
-# inha = 'a duck goes into a sea'
-# print(inha.replace('a', 'a nice'))
-#
-# pokemons_list = ['피카츄', '꼬부기', '이상해', '파이리']
-# pokemons_string = '/'.join(pokemons_list)
-# print(pokemons_string)
-#
-# univ = 'Inha University'
-# print(univ.split())
-#
-# print(len(univ))
-# print(univ[-10:-6])
-# print(univ[5:-6])
-# print(univ[::2])
-# print(univ[5:])
-# print(univ[5:15])
-# print(univ[-10:])
-#
+idx = song.find('m')
+print(idx, song[idx].upper())
