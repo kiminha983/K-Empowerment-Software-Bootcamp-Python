6abdd73febde73b69c292a2e811bb366753bc4ce
kiminha983 <kiminha983@gmail.com>
2023-01-16 15:35:37 +0900

    day03 prime number interval

--- day03.py
+++ day03.py
@@ -1,15 +1,18 @@
-# input 2 numbers
+# prime number
 
-# start = int(input("start number : "))
-# end = int(input("end number : "))
-# print(start, end)
+start = int(input("input start number : "))
+end = int(input("input end number : "))
 
-# start_end = input("start and end number : ").split()
-# print(start_end)
-# print(int(start_end[0]), int(start_end[1]))
+if end < start:
+    start, end = end, start
 
-start = int(input("start number : "))
-end = int(input("end number : "))
-print(start, end)
-for k in range(start, end+1):
-    print(k, end=' ')
+for i in range(start, end+1):
+    # is_prime = True
+    if i <= 1:
+        continue
+    for k in range(2, i):
+        if i % k == 0:
+            # is_prime = False
+            break
+    else:
+        print(i, end=' ')
