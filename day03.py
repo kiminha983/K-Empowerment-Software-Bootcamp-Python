8674d9d4e390e4c67fd716500fee14123ba788df
kiminha983 <kiminha983@gmail.com>
2023-01-16 10:19:59 +0900

    day03 text02

--- day03.py
+++ day03.py
@@ -5,3 +5,9 @@ print("I'm a\tboy")
 
 army = '우리는 국가와 국민에 충성을 다하는 대한민국 육군이다.\t하나 우리는 자유민주주의를 수호하며 조국 통일의 역군이 된다'
 print(army)
+
+start = '나 ' * 4 + '\n'
+middle = '헤이 ' * 3 + '\n'
+end = '안녕.'
+print(start + start + middle + end)
+
