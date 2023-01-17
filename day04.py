963c63cfb9971d193608c1e2033360c9ba9e1ceb
kiminha983 <kiminha983@gmail.com>
2023-01-17 14:57:00 +0900

    day04 dictionary 01

--- day04.py
+++ day04.py
@@ -1,15 +1,37 @@
-# ex 7-4, 7-5, 7-6, 7-7
-things = ["mozzarella", "cinderella", "salmonella"]
-things[-2] = things[-2].title()
-print(things)
-things[0] = things[0].upper()
-print(things)
-print(f'Delete the {things.pop()} from things, get Nobel Prize')
-print(things)
+# 안주 추천 프로그램 v0.1
+alcohol_foods = {
+    '맥주': '치킨',
+    '소주': '골뱅이 소면',
+    '위스키': '치즈',
+    '고량주': '짬뽕'
+}
+alcohol_list = list(alcohol_foods)  # extract keys
+while True:
+    alcohol = input(f'술을 선택 하세요 1) {alcohol_list[0]} 2) {alcohol_list[1]} 3) {alcohol_list[2]} 4) {alcohol_list[3]} 5) 계산 : ')
+    if alcohol == '5':
+        print('다음에 또 오세요~')
+        break
+    elif alcohol == '1':
+        print(f'{alcohol_list[0]}에 어울리는 안주는 {alcohol_foods[alcohol_list[0]]}입니다')
+    elif alcohol == '2':
+        print(f'{alcohol_list[1]}에 어울리는 안주는 {alcohol_foods[alcohol_list[1]]}입니다')
+    elif alcohol == '3':
+        print(f'{alcohol_list[2]}에 어울리는 안주는 {alcohol_foods[alcohol_list[2]]}입니다')
+    elif alcohol == '4':
+        print(f'{alcohol_list[3]}에 어울리는 안주는 {alcohol_foods[alcohol_list[3]]}입니다')
+    else:
+        print('메뉴 에서 골라 주세요.')
 
 
+# # dictionary
 
-
-# for thing in things:
-#     print(thing.title())
+# students = {'name': 'kim inha', 'age': 23, 'eyes': [0.9, 1.1]}
+# #for k in students.keys():
+# #for k in students:
+# #for k in students.values():
+# for k, v in students.items():
+#     # print(k)
+#     print(f'{k} : {v}')
+# print(f'이름은 {students["name"]}, 나이는 {students["age"]}세', end=' ')
+# print(f'시력은 좌: {students["eyes"][0]}, 우: {students["eyes"][1]}')
 
