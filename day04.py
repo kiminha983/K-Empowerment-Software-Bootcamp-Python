5a5edb04ffe2b9e555a7e5e69d1af667f8d477ad
kiminha983 <kiminha983@gmail.com>
2023-01-17 10:55:59 +0900

    day04 list01

--- day04.py
+++ day04.py
@@ -1,22 +1,34 @@
-# tuple
-a = 'harry',
-b = ('harry',)
-c = ()  # empty tuple
-d = 'harry', 'ron'  # packing
-e = ('hermione')  # string
-f = ('harry', 'ron')  # packing
-g = ('hermione',)  # string
-print(g, id(g))
-g = g + f  # concatenation
-print(g, id(g))
+# list
+big_bang = ['GD', '태양', '탑', '대성', '승리']
+exo = ['백현', '첸']
+# big_bang.append('인하')
+big_bang.insert(1, '인하')
+# print(big_bang * 2)
 
-print(f[1])
-x, y = f  # unpacking
-print(y)
+# exo.extend(big_bang)
+# exo = exo + big_bang
+exo.append(big_bang)
+print(exo)
+print(exo[2][2])  # 태양
+print(exo[-1][-4])  # 태양
+exo[-2] = '시우민'
+print(exo)
+# print(exo.pop())  # 빅뱅 pop
+print(exo[2].pop())  # 승리 pop
+print(exo)
+print(exo[2].pop(-2))  # 탑 pop
+print(exo)
+del exo[2][-1]  # 대성 delete
+print(exo)
+# exo.remove('인하')  # not in exo
+exo[2].remove('인하')
+print(exo)
 
-print(type(a))
-print(type(b))
-print(type(c))
-print(type(d))
-print(type(e))
-print(type(f))
+# scores = ('B+', 'A+', 'C+')
+# print(scores[1], scores[2])
+# temp = list(scores)
+# temp[1] = 'C+'
+# temp[2] = 'A+'
+# scores = tuple(temp)
+# print(scores[1], scores[2])
+
