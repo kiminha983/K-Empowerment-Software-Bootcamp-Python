dbe36f5d10662a9aedc3f330a7a443796583ce5c
kiminha983 <kiminha983@gmail.com>
2023-01-18 14:05:24 +0900

    day05 function 04

--- day05.py
+++ day05.py
@@ -1,13 +1,12 @@
-# v0.2
+# v0.2 return Dictionary
 import random
 
-# def calculate_fee(*args):
 
-def calculate_fee(args) -> list:
+def calculate_fee(args) -> dict:
     """
     놀이공원 요금 계산 프로그램
     :param args: ages in list
-    :return: [전체 인원 수, 어른 수, 아이 수, 지불할 총 입장료]
+    :return: {'no_of_people':전체 인원 수, 'no_of_adult':어른 수, 'no_of_kid':아이 수, 'total_fee':지불할 총 입장료}
     """
     total = 0
     adults = 0
@@ -19,10 +18,10 @@ def calculate_fee(args) -> list:
         else:
             total = total + 3000
             kids = kids + 1
-    return [len(args), adults, kids, total]
+    return {'no_of_people': len(args), 'no_of_adult': adults, 'no_of_kid': kids, 'total_fee': total}
 
 
 no_of_visitor = int(input('몇 분 이세요? '))
 ages = [random.randint(1, 60) for age in range(no_of_visitor)]
 results = calculate_fee(ages)
-print(f'{results[0]}명 방문 하셨고 어른 {results[1]}명, 아이 {results[2]}명 총 요금은 {results[-1]}원 입니다')
+print(f"{results['no_of_people']}명 방문 하셨고 어른 {results['no_of_adult']}명, 아이 {results['no_of_kid']}명 총 요금은 {results['total_fee']}원 입니다")
