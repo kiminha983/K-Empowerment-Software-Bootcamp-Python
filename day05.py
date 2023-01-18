1a87397c1ab3326d8ee870bac59ccded8b5c5866
kiminha983 <kiminha983@gmail.com>
2023-01-18 15:33:38 +0900

    day05 function 06

--- day05.py
+++ day05.py
@@ -1,41 +1,20 @@
-# function
-
-def inha():
-    """
-    숫자 출력
-    :return:
-    """
-    print(60)
-
-
-def call_func(f):
-    """
-    매개변수로 함수를 넘겨 받아 실행
-    :param f: 매개변수가 함수
-    :return:
-    """
-    f()  # 넘겨 받은 함수 실행
+# Closures
+def calculate():
+    x = 1
+    y = 2
+    temp = 0
+    def add_sub(n):
+        nonlocal temp
+        # x = 11  # local variable
+        temp = temp + x + n - y
+        return temp
+    print('once')
+    return add_sub
 
 
-def subtract(n1, n2):
-    print(n1 - n2)
+c1 = calculate()
+for i in range(5):
+    print(c1(i))
 
-
-def run_func(f, arg1, arg2):
-    """
-    함수를 매개 변수로 받아 함수 안에서 해당 함수를 실행
-    :param f: 첫 번째 인수는 함수
-    :param arg1: 정수 값
-    :param arg2: 정수 값
-    :return:
-    """
-    f(arg1, arg2)
-
-
-a = (5, 7, -11)
-print(sum(a))
-
-# run_func(subtract, 99, 88)
-#
-# call_func(inha)
-# print(type(call_func))
+print(type(c1))
+print(c1)
