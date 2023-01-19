cdd7681c420e8ebc4e7076ba664eabbe92562a2b
kiminha983 <kiminha983@gmail.com>
2023-01-19 11:21:51 +0900

    day06 function 11

--- day06.py
+++ day06.py
@@ -1,22 +1,18 @@
-def document_info(func):
-    def new_function(*args, **kwargs):
-        print('실행 중인 함수:', func.__name__)
-        print('위치 기반 인수들:', args)
-        print('키워드 기반 인수들:', kwargs)
-        result = func(*args, **kwargs)
-        print('실행 결과:', result)
-        return result
-    return new_function
-
-@document_info
-def sub_int(x, y):
-    return x - y
-
-@document_info
-def squares(n):
-    return n * n
+g = 1  # global variable
+def print_global():
+    # g = 1  # local variable
+    print(g)
 
 
-print(sub_int(7, 3))
-print(squares(5))
+def change_print_global():
+    global g
+    print(g)
+    g = 2
+    print(g)
 
+
+change_print_global()
+print_global()
+print(g)
+print(globals())
+print(__name__)
