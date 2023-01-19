93c58a5d535bbb1ce25dde63e4e3a50cf1344f32
kiminha983 <kiminha983@gmail.com>
2023-01-19 10:36:35 +0900

    day06 function 09

--- day06.py
+++ day06.py
@@ -1,20 +1,20 @@
-def a():  # generator
-    yield 1
-    yield 2
-    yield 3
+def sub_int(x, y):
+    return x - y
 
 
-def b():  # normal function
-    return 1  # stop
-    return 2
-    return 3
+# decorator
+def document_info(func):
+    def new_function(*args, **kwargs):
+        print('실행 중인 함수:', func.__name__)
+        print('위치 기반 인수들:', args)
+        print('키워드 기반 인수들:', kwargs)
+        result = func(*args, **kwargs)
+        print('실행 결과:', result)
+        return result
+    return new_function
 
 
-print(a(), b())
-c = a()
-print(c)
-for i in c:
-    print(i)
-
-for i in c:
-    print(i)
+print(sub_int(7, 3))
+info_sub_int = document_info(sub_int)
+r = info_sub_int(7, 3)
+print(r)
