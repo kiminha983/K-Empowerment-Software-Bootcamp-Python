c1182b83f30f0acad4b13b274b48221c8008ab5e
kiminha983 <kiminha983@gmail.com>
2023-01-19 15:40:36 +0900

    day06 class 01

--- day06.py
+++ day06.py
@@ -1,21 +1,33 @@
-# ex9-2
-cnts = 0
-def get_odds():
-    for num in range(10):
-        if num % 2:
-            yield num
+# class
+
+class Pokemon:
+    def __init__(self, name, owner, skills):
+        self.name = name
+        self.owner = owner
+        self.skills = skills.split('/')
+        print(f"포켓몬 {name} 생성됨")
+
+    def info(self):
+        print(f"{self.owner}의 포켓몬은 {self.name}입니다")
+        for skill in self.skills:
+            print(skill)
 
 
-odds = get_odds()
-for i in odds:
-    cnts += 1
-    if cnts == 3:
-        print(i)
+
+p1 = Pokemon('피카츄', '한지우', '50만 볼트/100만 볼트/번개')
+p2 = Pokemon('꼬부기', '오바람', '고속스핀/거품/몸통박치기/하이드로펌프')
+
+p2.info()
+p1.info()
+# print(p2.skills)
+# print(p1, p2)
 
 
-# ex9-1
-
-# def good():
-#     return ['Harry', 'Ron', 'Hermione']
+# class Pokemon:
+#     def __init__(self):  # 객체 생성 시 동작
+#         print("포켓몬 객체 생성됨")
 #
-# print(good())
+#
+# p1 = Pokemon()
+# p2 = Pokemon()
+# print(p1, p2)
