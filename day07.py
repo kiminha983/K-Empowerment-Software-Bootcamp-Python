196c822a7beeb61bd5da55ebf2ec71026fb03ee4
kiminha983 <kiminha983@gmail.com>
2023-01-20 11:32:12 +0900

    day07 class 04 pokemon game v0.1

--- day07.py
+++ day07.py
@@ -8,8 +8,11 @@ class Pokemon:
 
     def info(self):
         print(f"{self.owner}의 포켓몬이 사용 가능한 스킬")
-        for skill in self.skills:
-            print(skill)
+        for i in range(len(self.skills)):
+            print(f'{i+1} : {self.skills[i]}')
+
+        # for skill in self.skills:
+        #     print(f'{skill}')
 
     def attack(self, idx):
         print(f'{self.skills[idx]} 공격 시전!')
@@ -38,19 +41,39 @@ class Ggoboogi(Pokemon):  # inheritance
         print(f'{self.name}가 수영을 합니다')
 
 
-p0 = Pokemon('아이리스', '어떤공격')
-p0.attack(0)
-# p0.swim()  # 꼬부기 클래스의 객체들이 사용할 수 있는 고유 메서드
+class Pairi(Pokemon):  # inheritance
+    def __init__(self, owner, skills):
+        super().__init__(owner, skills)
+        self.name = "파이리"
+        print(f"{self.name}")
 
-pk1 = Pikachu('한지우', '번개/100만 볼트')
-#pk1.info()
-ggo1 = Ggoboogi('오바람', '고속스핀/거품/몸통박치기')
-#ggo1.info()
-ggo1.swim()
-ggo1.attack(2)
-pk1.attack(1)
+    def attack(self, idx):  # override
+        print(f'{self.owner}의 {self.name}가 {self.skills[idx]} 공격(불) 시전!')
 
 
-#pi1.info()
-# p1 = Pokemon('한지우', '50만 볼트/100만 볼트/번개')
-# p2 = Pokemon('오바람', '고속스핀/거품/몸통박치기/하이드로펌프')
+while True:
+    menu = input('1) 포켓몬 생성  2) 프로그램 종료 : ')
+    if menu == '2':
+        print('프로그램을 종료합니다')
+        break
+    elif menu == '1':
+        pokemon = input('1) 피카츄  2) 꼬부기  3) 파이리 : ')
+        if pokemon == '1':
+            n = input('플레이어 이름 입력 : ')
+            s = input('사용 가능한 기술 입력(/로 구분하여 입력) :')
+            p = Pikachu(n, s)
+        elif pokemon == '2':
+            n = input('플레이어 이름 입력 : ')
+            s = input('사용 가능한 기술 입력(/로 구분하여 입력) :')
+            p = Ggoboogi(n, s)
+        elif pokemon == '3':
+            n = input('플레이어 이름 입력 : ')
+            s = input('사용 가능한 기술 입력(/로 구분하여 입력) :')
+            p = Pairi(n, s)
+        else:
+            print('메뉴에서 골라 주세요')
+        p.info()
+        attack_menu = input('공격 번호 선택 : ')
+        p.attack(int(attack_menu)-1)
+    else:
+        print('메뉴에서 골라 주세요')
