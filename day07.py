c5f2c564c82a843a03de19799a981b24142526a0
kiminha983 <kiminha983@gmail.com>
2023-01-20 11:46:40 +0900

    day07 class 05

--- day07.py
+++ day07.py
@@ -1,79 +1,22 @@
-# class
+class PrettyMixin:
+    def time_print(self):
+        import datetime
+        print(datetime.date.today())
 
-class Pokemon:
-    def __init__(self, owner, skills):
-        self.owner = owner
-        self.skills = skills.split('/')
-        print(f"포켓몬 생성 :", end=' ')
-
-    def info(self):
-        print(f"{self.owner}의 포켓몬이 사용 가능한 스킬")
-        for i in range(len(self.skills)):
-            print(f'{i+1} : {self.skills[i]}')
-
-        # for skill in self.skills:
-        #     print(f'{skill}')
-
-    def attack(self, idx):
-        print(f'{self.skills[idx]} 공격 시전!')
+    def dump(self):
+        import pprint
+        pprint.pprint(vars(self))
 
 
-class Pikachu(Pokemon):  # inheritance
-    def __init__(self, owner, skills):
-        super().__init__(owner, skills)
-        self.name = "피카츄"
-        print(f"{self.name}")
-
-    def attack(self, idx):  # override
-        print(f'{self.owner}의 {self.name}가 {self.skills[idx]} 공격(전기) 시전!')
+class Thing(PrettyMixin):
+    pass
 
 
-class Ggoboogi(Pokemon):  # inheritance
-    def __init__(self, owner, skills):
-        super().__init__(owner, skills)
-        self.name = "꼬부기"
-        print(f"{self.name}")
-
-    def attack(self, idx):  # override
-        print(f'{self.owner}의 {self.name}가 {self.skills[idx]} 공격(물) 시전!')
-
-    def swim(self):
-        print(f'{self.name}가 수영을 합니다')
-
-
-class Pairi(Pokemon):  # inheritance
-    def __init__(self, owner, skills):
-        super().__init__(owner, skills)
-        self.name = "파이리"
-        print(f"{self.name}")
-
-    def attack(self, idx):  # override
-        print(f'{self.owner}의 {self.name}가 {self.skills[idx]} 공격(불) 시전!')
-
-
-while True:
-    menu = input('1) 포켓몬 생성  2) 프로그램 종료 : ')
-    if menu == '2':
-        print('프로그램을 종료합니다')
-        break
-    elif menu == '1':
-        pokemon = input('1) 피카츄  2) 꼬부기  3) 파이리 : ')
-        if pokemon == '1':
-            n = input('플레이어 이름 입력 : ')
-            s = input('사용 가능한 기술 입력(/로 구분하여 입력) :')
-            p = Pikachu(n, s)
-        elif pokemon == '2':
-            n = input('플레이어 이름 입력 : ')
-            s = input('사용 가능한 기술 입력(/로 구분하여 입력) :')
-            p = Ggoboogi(n, s)
-        elif pokemon == '3':
-            n = input('플레이어 이름 입력 : ')
-            s = input('사용 가능한 기술 입력(/로 구분하여 입력) :')
-            p = Pairi(n, s)
-        else:
-            print('메뉴에서 골라 주세요')
-        p.info()
-        attack_menu = input('공격 번호 선택 : ')
-        p.attack(int(attack_menu)-1)
-    else:
-        print('메뉴에서 골라 주세요')
+t = Thing()
+t.time_print()
+t.name = "Nyarlathotep"
+t.feature = "ichor"
+t.age = "eldritch"
+t.gender = "female"
+t.gender = "male"
+t.dump()
