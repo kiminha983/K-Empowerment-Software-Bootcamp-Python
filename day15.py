c830176e645c31e502079864637b49fe2d028cfd
kiminha983 <kiminha983@gmail.com>
2023-02-03 11:29:56 +0900

    day15 linear list 03

--- day15.py
+++ day15.py
@@ -12,10 +12,27 @@ def insert_data(idx, pokemon):
     pokemons[idx] = pokemon
 
 
+def delete_data(idx):
+    if idx < 0 or idx > len(pokemons):
+        print("Out of range!")
+        return
+
+    len_pokemons = len(pokemons)
+    pokemons[idx] = None
+
+    for i in range(idx + 1, len_pokemons):
+        pokemons[i - 1] = pokemons[i]
+        pokemons[i] = None
+
+    del (pokemons[len_pokemons - 1])
+
+
 if __name__ == "__main__":
     pokemons = ["피카츄", "라이츄", "꼬부기", "파이리", "이상해"]
     print(pokemons)
-    insert_data(3, '어니부기')
+    #insert_data(3, '어니부기')
+    delete_data(1)
     print(pokemons)
-    insert_data(6, '거북왕')
+    #insert_data(6, '거북왕')
+    delete_data(3)
     print(pokemons)
