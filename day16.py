c153050101251e77da13fc453671fecbd6cb9564
kiminha983 <kiminha983@gmail.com>
2023-02-06 10:40:41 +0900

    day16 linked list 02

--- day16.py
+++ day16.py
@@ -1,50 +1,7 @@
-# def find_and_insert_data(pokemon, k_count) :
-# 	findPos = -1
-# 	for i in range(len(pokemons)):
-# 		pair = pokemons[i]
-# 		if k_count <= pair[1]:
-# 			findPos = i
-# 			break
-# 	if findPos == -1:
-# 		findPos = len(pokemons)
-#
-# 	insert_data(findPos, [pokemon, k_count])
-#
-#
-# def insert_data(position, pokemon):
-# 	if position < 0 or position > len(pokemons):
-# 		print("데이터를 삽입할 범위를 벗어났습니다.")
-# 		return
-#
-# 	pokemons.append(None)
-# 	kLen = len(pokemons)
-#
-# 	for i in range(kLen - 1, position, -1):
-# 		pokemons[i] = pokemons[i - 1]
-# 		pokemons[i - 1] = None
-#
-# 	pokemons[position] = pokemon
-#
-#
-#
-# pokemons = [['피카츄', 300], ['꼬부기', 500], ['어니부기', 700], ['거북왕', 1000]]
-#
-# ## 메인 코드 부분 ##
-# if __name__ == "__main__":
-#
-# 	while True:
-# 		pokemon = input("새로운 몬스터 : ")
-# 		hp = int(input("체력 : "))
-# 		find_and_insert_data(pokemon, hp)
-# 		print(pokemons)
-
 class Node:
-	def __init__(self):
-		self.data = None
-		self.link = None
-
-	# def __repr__(self):
-	# 	return f'포켓몬스터!'
+    def __init__(self, data):
+        self.data = data
+        self.link = None
 
 
 def print_nodes(start):
@@ -53,30 +10,96 @@ def print_nodes(start):
         return
     print(current.data, end=' ')
     while current.link != None:
-        current = current.link  # 다음 노드로 이동
+        current = current.link
         print(current.data, end=' ')
     print()
 
 
-memory = []
+def insert_nodes(find_data, insert_data):
+    global head, current, pre
+    if head.data == find_data:
+        node = Node(insert_data)
+        node.link = head
+        head = node
+        return
+
+    current = head
+    while current.link != None:
+        pre = current
+        current = current.link
+        if current.data == find_data:
+            node = Node(insert_data)
+            node.link = current
+            pre.link = node
+            return
+
+    node = Node(insert_data)
+    current.link = node
+
+
+def delete_nodes(delete_data):
+    global head, current, pre
+
+    if head.data == delete_data:
+        print('첫 번째 노드 삭제 완료')
+        current = head
+        head = head.link
+        del current
+        return
+
+    current = head
+    while current.link != None:
+        pre = current
+        current = current.link
+        if current.data == delete_data:
+            print('중간 노드 삭제 완료')
+            pre.link = current.link
+            del current
+            return
+
+    print('삭제할 노드를 찾지 못함')
+
+
+def find_nodes(find_data):
+    global head, current, pre
+
+    current = head
+    if current.data == find_data:
+        return current
+
+    while current.link != None:
+        current = current.link
+        if current.data == find_data:
+            return current
+
+    return Node(None)
+
+
 head, current, pre = None, None, None
-dataArray = ["피카츄", "라이츄", "꼬부기", "파이리", "이상해"]
+data_array = ["피카츄", "라이츄", "꼬부기", "파이리", "이상해"]
 
 
 if __name__ == "__main__":
-    node = Node()
-    node.data = dataArray[0]
+    node = Node(data_array[0])
     head = node
-    memory.append(node)
 
-    for data in dataArray[1:]:
+    for data in data_array[1:]:
         pre = node
-        node = Node()
-        node.data = data
+        node = Node(data)
         pre.link = node
-        memory.append(node)
 
     print_nodes(head)
-    print(memory)
-    # print(node.data)
-    # print(pre.data)
+    insert_nodes("피카츄", "잠만보")
+    print_nodes(head)
+    insert_nodes("파이리", "어니부기")
+    print_nodes(head)
+    insert_nodes("성윤모", "거북왕")
+    print_nodes(head)
+    delete_nodes("잠만보")
+    print_nodes(head)
+    delete_nodes("어니부기")
+    print_nodes(head)
+    delete_nodes("강찬석")
+    print_nodes(head)
+    print(find_nodes("파이리").data)
+    print(find_nodes("박민석").data)
